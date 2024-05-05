c0cde01f972d339d334e036062f3c2d2353d859e
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-05 16:31:31 +0900

    Revert "Top about"

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,3 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins, controllers:{
-    sessions: 'admin/sessions'
-  }
-  
-  namespace :admin do
-    resources :users, only: [:index, :show, :edit, :update, :destroy]
-    resources :posts, only: [:index, :show, :destroy] do
-      resources :post_comment, only: [:destroy]
-    end
-    resources :genres, only: [:index, :create, :edit, :update]
-  end
-  
-  devise_for :users, controllers: {
-    registrations: 'public/registrations',
-    sessions: 'public/sessions'
-  }
-  
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
-  
-  scope module: :public do
-    root 'homes#top'
-    get '/about' => 'homes#about'
-    get '/tag_searches/search' => 'tag_searches#search'
-    get '/searches/search' => 'searches#search'
-    resources :users, only: [:index, :edit, :show, :update, :destroy]
-    resources :posts do
-      resources :post_commets, only: [:create, :destroy]
-    end
-  end
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
