b4a99b867d978a5f9ea0679f5d3fe36ba11d7a20
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-05 12:50:14 +0000

    add sign_in,up

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,17 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def prohibit_multiple_login
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins, controllers:{
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers:{
     sessions: 'admin/sessions'
   }
-  
+
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update, :destroy]
     resources :posts, only: [:index, :show, :destroy] do
@@ -11,16 +20,7 @@ Rails.application.routes.draw do
     end
     resources :genres, only: [:index, :create, :edit, :update]
   end
-  
-  devise_for :users, controllers: {
-    registrations: 'public/registrations',
-    sessions: 'public/sessions'
-  }
-  
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
-  
+
   scope module: :public do
     root 'homes#top'
     get '/about' => 'homes#about'
@@ -31,6 +31,6 @@ Rails.application.routes.draw do
       resources :post_commets, only: [:create, :destroy]
     end
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
