2e47425dee190dc70266fd0edae323e1fddd4902
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-06 11:36:49 +0000

    [add]guest,admin-user追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -36,5 +36,10 @@ class Public::SessionsController < Devise::SessionsController
   def prohibit_multiple_login
     redirect_to root_path
   end
-
+  
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "ゲストとしてログインしました"
+  end
 end
