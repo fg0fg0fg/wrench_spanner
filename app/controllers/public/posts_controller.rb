31cd8f411c93174f162908972b3e113b7f952fb3
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-06 13:10:24 +0000

    [add]model,controller追記

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,67 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :matching_login_user, only: [:edit, :update, :destroy]
+  before_action :ensure_guest_user, only: [:new, :edit, :create, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post), notice: "レビューを投稿しました"
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @post_comment = PostComment.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "レビューを更新しました"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user), notice: "レビューを削除しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :star, :tag, :post_image)
+  end
+
+  def ensure_guest_user
+    if current_user.email == "guest@example.com"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーは閲覧のみです"
+    end
+  end
+
+  def matching_login_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
-  
+  belongs_to :genre
+  has_many :post_comments, dependent: :destroy
+
+  has_one_attached :post_image
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+
 end
