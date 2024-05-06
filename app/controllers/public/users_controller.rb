1af8537625e8427c60605acdbace5db9c890dcd6
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-06 04:44:51 +0000

    [add]user関連追記

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,45 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :matching_login_user, only: [:edit, :update, :destroy]
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email)
+  end
+  
+  def matching_login_user
+    @user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
 end
