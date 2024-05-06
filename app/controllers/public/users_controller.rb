67b55c37b9752f78aecc57679f4ad0ccf7e58ef8
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-06 09:31:01 +0000

    [add]user-view

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -38,7 +38,7 @@ class Public::UsersController < ApplicationController
   
   def matching_login_user
     @user = User.find(params[:id])
-    unless user.id == current_user.id
+    unless @user.id == current_user.id
       redirect_to user_path(current_user)
     end
   end
