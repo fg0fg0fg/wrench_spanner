de3d24f60ebbcc7a3556bab00d415e23d7da3312
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-09 09:05:15 +0000

    [add]

--- db/migrate/20240503075753_create_posts.rb
+++ db/migrate/20240503075753_create_posts.rb
@@ -2,11 +2,11 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.integer :user_id, null: false
-      #t.integer :genre_id, null: false
+      t.integer :genre_id, null: false
       t.string :title, null: false
       t.text :body, null: false
-      #t.string :star, null: false
-      #t.string :tag
+      t.string :star, null: false
+      t.string :tag
 
       t.timestamps
     end
