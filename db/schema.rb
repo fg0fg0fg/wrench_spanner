594c2b9bac53d2adf619e61d0cfcbb6845149e6b
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-11 09:53:41 +0000

    [update]post_commet=>comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_03_083533) do
+ActiveRecord::Schema.define(version: 2024_05_11_094355) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,16 +52,16 @@ ActiveRecord::Schema.define(version: 2024_05_03_083533) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
-    t.text "comment", null: false
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/controllers/admin/comments_controller_test.rb
+++ test/controllers/admin/comments_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Admin::PostCommentsControllerTest < ActionDispatch::IntegrationTest
+class Admin::CommentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
