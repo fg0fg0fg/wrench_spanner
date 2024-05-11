b3279e0d7858ba45b8664735c7d6eacc20a423ac
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-11 09:23:30 +0000

    [add]search関連,部分テンプレート化

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
-  
-  
+  before_action :login_check
+
   def search
     @model = params[:model]
     @content = params[:content]
@@ -9,6 +9,14 @@ class SearchesController < ApplicationController
       @records = User.search_for(@content, @method)
     else
       @records = Post.search_for(@content, @method)
+    end
+  end
+
+  private
+
+  def login_check
+    unless user_signed_in? || admin_signed_in?
+      redirect_to root_path
     end
   end
 end
