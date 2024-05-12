209266ff56a7e0cba9fde27a5e90022766cc53d7
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-05-12 07:07:20 +0000

    [update]非同期化に修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,13 +6,11 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
     @comment.save
-    redirect_to request.referer
   end
   
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to request.referer
   end
   
   private
